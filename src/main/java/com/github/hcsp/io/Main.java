3454a8fa3f8877d634b856e8466e9bd90a719c83
concise <15521608275@163.com>
2020-06-27 10:18:30 +0800

    出现Disconnected from the target VM
    半天没能解决

--- src/main/java/com/github/hcsp/io/Main.java
+++ src/main/java/com/github/hcsp/io/Main.java
@@ -6,22 +6,68 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("http://sina.cn");
+    public static void main(String[] args) throws IOException {
 
-        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-            System.out.println(response1.getStatusLine());
-            HttpEntity entity1 = response1.getEntity();
-            // do something useful with the response body
-            // and ensure it is fully consumed
-            System.out.println(EntityUtils.toString(entity1));
-        } catch (IOException e) {
-            e.printStackTrace();
+        // 待处理的链接池
+        List<String> linkPool = new ArrayList<>();
+        //已经处理的链接池
+        Set<String> processedLinks = new HashSet<>();
+        linkPool.add("https://sina.cn");
+
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+
+            String link = linkPool.remove(linkPool.size() - 1);
+
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+
+            if (!link.contains("sina.com")) {
+                //这是我们不感兴趣的
+                continue;
+            } else {
+                CloseableHttpClient httpclient = HttpClients.createDefault();
+                HttpGet httpGet = new HttpGet(link);
+
+                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+                    System.out.println(link);
+                    System.out.println(response1.getStatusLine());
+                    HttpEntity entity1 = response1.getEntity();
+
+                    String html = EntityUtils.toString(entity1);
+                    //这是我们感兴趣的“新闻”，不感兴趣的广告不包含在内
+
+                    Document doc = Jsoup.parse(html);
+
+                    ArrayList<Element> links = doc.select("a");
+
+                    for (Element aTag : links) {
+                        linkPool.add(aTag.attr("href"));
+                    }
+                    //假如这是一个新闻的详情，就存入数据库
+                    ArrayList<Element> articleTags = doc.select("article");
+                    if (!articleTags.isEmpty()) {
+                        for (Element articleTag : articleTags) {
+                            System.out.println("");
+                        }
+
+                    }
+                }
+            }
         }
     }
 }
